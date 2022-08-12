2b593bcc8aa58568481544eedfa6d141a2a1f92a
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-08-13 03:14:27 +0530

    Added changing password functionality.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Switch, Route } from 'react-router-dom'
+
 import Layout from './components/Layout/Layout'
 import UserProfile from './components/Profile/UserProfile'
 import AuthPage from './pages/AuthPage'
